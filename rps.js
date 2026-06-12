39fa56764e81416ad48771e3a762d475a9d4b33e
eatmyshorts5 <daniel.chen@utexas.edu>
2026-06-11 23:34:51 -0500

    Finish rps ui

--- rps.js
+++ rps.js
@@ -1,38 +1,47 @@
 let humanScore = 0;
 let computerScore = 0;
+const humanChoice = document.querySelector("#rps-choice");
+const resultText = document.querySelector("#result");
+const humanScoreText = document.querySelector("#human-score");
+const computerScoreText = document.querySelector("#computer-score");
 const validOptions = ["Rock", "Paper", "Scissors"];
 
-function getHumanChoice() {
-  let humanChoice;
-
-  for (;;) {
-    humanChoice = prompt("Type RPS hand");
-    humanChoice =
-      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-    if (validOptions.includes(humanChoice)) {
-      return humanChoice;
-    }
-    console.log("Invalid input, please try again");
-  }
+function playGame(event) {
+  let humanHand = event.target.dataset.choice;
+  let computerHand = getComputerChoice();
+  playRound(computerHand, humanHand);
 }
 
 function playRound(computerHand, humanHand) {
   if (humanHand === computerHand) {
     console.log("TIE!");
+    resultText.textContent = "TIE!";
   } else if (
     (humanHand === "Rock" && computerHand === "Scissors") ||
     (humanHand === "Paper" && computerHand === "Rock") ||
     (humanHand === "Scissors" && computerHand === "Paper")
   ) {
     console.log(`You win! ${humanHand} beats ${computerHand}!`);
+    resultText.textContent = `You win! ${humanHand} beats ${computerHand}!`;
     humanScore++;
+    humanScoreText.textContent = `Human Score: ${humanScore}`;
   } else if (
     (computerHand === "Rock" && humanHand === "Scissors") ||
     (computerHand === "Paper" && humanHand === "Rock") ||
     (computerHand === "Scissors" && humanHand === "Paper")
   ) {
     console.log(`You lose! ${computerHand} beats ${humanHand}!`);
+    resultText.textContent = `You lose! ${computerHand} beats ${humanHand}!`;
     computerScore++;
+    computerScoreText.textContent = `Computer Score: ${computerScore}`;
+  }
+
+  if (humanScore === 5 || computerScore === 5) {
+    resultText.textContent = `Game Over. Final Score. You: ${humanScore} Computer: ${computerScore}`;
+    humanScore = 0;
+    computerScore = 0;
+    computerScoreText.textContent = "Computer Score: 0";
+    humanScoreText.textContent = "Human Score: 0";
   }
 }
 
@@ -41,15 +50,4 @@ function getComputerChoice() {
   return validOptions[tempCode - 1];
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanHand = getHumanChoice();
-    let computerHand = getComputerChoice();
-    playRound(humanHand, computerHand);
-  }
-  console.log(
-    `Game Over. Final Score. You: ${humanScore} Computer: ${computerScore}`,
-  );
-}
-
-playGame();
+humanChoice.addEventListener("click", playGame);
